85185008310fc2cf395e46c6d66c02f62d179282
MariaPolina <by.maria.polina@gmail.com>
2023-05-09 22:51:16 +0300

    feat: 04 task ready

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,47 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir() {
+    const sourceDir = path.join(__dirname, '..', '/04-copy-directory/files');
+    const destinationDir = path.join(__dirname, '..', '/04-copy-directory/files-copy');
+
+    try {
+        await fs.access(destinationDir);
+    } catch (error) {
+        await fs.mkdir(destinationDir, { recursive: true });
+    }
+
+    const files = await fs.readdir(sourceDir);
+    for (const file of files) {
+        const sourcePath = path.join(sourceDir, file);
+        const destinationPath = path.join(destinationDir, file);
+        const stats = await fs.stat(sourcePath);
+        if (stats.isFile()) {
+            await fs.copyFile(sourcePath, destinationPath);
+        } else if (stats.isDirectory()) {
+            await copyDirRecursive(sourcePath, destinationPath);
+        }
+    }
+}
+
+async function copyDirRecursive(sourceDir, destinationDir) {
+    try {
+        await fs.access(destinationDir);
+    } catch (error) {
+        await fs.mkdir(destinationDir, { recursive: true });
+    }
+
+    const files = await fs.readdir(sourceDir);
+    for (const file of files) {
+        const sourcePath = path.join(sourceDir, file);
+        const destinationPath = path.join(destinationDir, file);
+        const stats = await fs.stat(sourcePath);
+        if (stats.isFile()) {
+            await fs.copyFile(sourcePath, destinationPath);
+        } else if (stats.isDirectory()) {
+            await copyDirRecursive(sourcePath, destinationPath);
+        }
+    }
+}
+
+copyDir();
